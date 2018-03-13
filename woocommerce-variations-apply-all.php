edee465222cd71b2eab1bf43b31412bac9ca32f4
Lucas Karpiuk <lucas@gowp.com>
2018-03-13 11:59:42 -0400

    updated to duplicate functionality for sale price

--- woocommerce-variations-apply-all.php
+++ woocommerce-variations-apply-all.php
@@ -3,7 +3,7 @@
 Plugin Name: GoWP WooCommerce Variations Apply All
 Plugin URL: https://www.gowp.com/blog/apply-the-price-of-a-woocommerce-product-variation-to-all-variations-of-the-same-product/
 Description: Adds a link to variation edit screen to apply a price to all variations
-Version:     1.0
+Version:     1.0.1
 Author:      GoWP
 Author URI:  https://www.gowp.com
 */
@@ -16,14 +16,23 @@ function gowp_global_variation_price() {
 		<script type="text/javascript">
 			function addVariationLinks() {
 				a = jQuery( '<a href="#">Apply to all Variations</a>' );
-				b = jQuery( 'input[name^="variable_regular_price"]' );
+				b = jQuery( 'input[name^="variable_regular_price"].wc_input_price' );
 				a.click( function( c ) {
-					d = jQuery( this ).parent( 'label' ).next( 'input[name^="variable_regular_price"]' ).val();
+					d = jQuery( this ).parent( 'label' ).next( 'input[name^="variable_regular_price"].wc_input_price' ).val();
 					e = confirm( "Change the price of all variations to " + d + "?" );
 					if ( e ) b.val( d ).trigger( 'change' );
 					c.preventDefault();
 				} );
 				b.prev( 'label' ).append( " " ).append( a );
+				aa = jQuery( '<a href="#">Apply to all Variations</a>' );
+				bb = jQuery( 'input[name^="variable_sale_price"].wc_input_price' );
+				aa.click( function( cc ) {
+					dd = jQuery( this ).parent( 'label' ).next( 'input[name^="variable_sale_price"].wc_input_price' ).val();
+					ee = confirm( "Change the price of all variations to " + dd + "?" );
+					if ( ee ) bb.val( dd ).trigger( 'change' );
+					cc.preventDefault();
+				} );
+				bb.prev( 'label' ).append( " " ).append( aa );
 			}
 			<?php if ( version_compare( $woocommerce->version, '2.4', '>=' ) ) : ?>
 				jQuery( document ).ready( function() {
